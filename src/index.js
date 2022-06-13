fa46f09bbb00502ba06c636e624af9d54f4b8db6
leo <2279846752@qq.com>
2022-06-13 22:53:05 +0800

    添加install方法

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ import TypeFn from './type';
 import DateFn from './date';
 import StorageFn from './storage';
 
-export { ArrayFn, NumberFn, StringFn, TypeFn, DateFn, StorageFn };
+const install = (prefix="_default", isEncrypt=true) => {
+  Array.prototype._jsUtil = new ArrayFn();
+  Number.prototype._jsUtil = new NumberFn();
+  String.prototype._jsUtil = new StringFn();
+  Date.prototype._jsUtil = new DateFn();
+  Object.prototype._jsUtil = new TypeFn();
+  Storage.prototype._jsUtil = new StorageFn({
+    prefix,
+    isEncrypt
+  });
+}
+
+export { ArrayFn, NumberFn, StringFn, TypeFn, DateFn, StorageFn, install };
